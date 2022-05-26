ac74df611c9d65829d61186bfeecfaa22d5b4e1a
Alfonso Gallardo <alfonsogallardo7@gmail.com>
2022-05-27 00:19:59 +0200

    Creando el proyecto web

---
+++ web/aep-app-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './modules/material-imports.module';
+import { AuthLoginComponent } from './components/auth-login/auth-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
