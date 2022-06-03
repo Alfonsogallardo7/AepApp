4a563ed5667d9e5dc5f493e6f41c6d4b6cea39de
Alfonso Gallardo <alfonsogallardo7@gmail.com>
2022-06-03 11:52:29 +0200

    Añado login

--- web/aep-app-angular/src/app/app-routing.module.ts
+++ web/aep-app-angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthLoginComponent } from './components/auth-login/auth-login.component';
+import { CampeonatoListComponent } from './components/campeonato-list/campeonato-list.component';
 
 const routes: Routes = [
-  // {path: '', component: AuthLoginComponent, redirectTo: 'auth/admin-login', pathMatch: 'full'},
-  {path: 'auth/admin-login', component: AuthLoginComponent}
+  // {path: 'auth/admin-login', component: AuthLoginComponent },
+  {path: 'championships/', component: CampeonatoListComponent},
+   {path: '', redirectTo: 'championships/', pathMatch: 'full'},
 ];
 
 @NgModule({
