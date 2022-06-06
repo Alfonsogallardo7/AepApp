56b2b6f2eaabda5a22d0de3874178813a2824719
Alfonso Gallardo <alfonsogallardo7@gmail.com>
2022-06-06 12:15:04 +0200

    Añado el bloc de perfil

--- mobile/aep_app/lib/repository/auth_repository/auth_repository.dart
+++ mobile/aep_app/lib/repository/auth_repository/auth_repository.dart
@@ -1,9 +1,11 @@
 import 'package:aep_app/models/login_response.dart';
+import 'package:aep_app/models/me_response.dart';
 import 'package:aep_app/models/register_dto.dart';
 import 'package:aep_app/models/register_response.dart';
 
 abstract class AuthRepository {
   Future<LoginResponse> login(LoginDto loginDto);
-    Future<RegisterResponse> register (RegisterDto registerDto, String imagePath);
+  Future<RegisterResponse> register (RegisterDto registerDto, String imagePath);
+  Future<MeResponse> me ();
 
 }
--- mobile/aep_app/lib/repository/auth_repository/auth_repository_impl.dart
+++ mobile/aep_app/lib/repository/auth_repository/auth_repository_impl.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
 import 'package:aep_app/models/login_response.dart';
+import 'package:aep_app/models/me_response.dart';
 import 'package:aep_app/models/register_response.dart';
 import 'package:aep_app/models/register_dto.dart';
 import 'package:aep_app/repository/auth_repository/auth_repository.dart';
+import 'package:aep_app/repository/constants.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthrepositoryImpl extends AuthRepository {
   final Client _client = Client();
@@ -65,6 +68,19 @@ class AuthrepositoryImpl extends AuthRepository {
           jsonDecode(await response.stream.bytesToString()));
     } else {
       throw Exception('Failed to register');
+    }
+  }
+
+  @override
+  Future<MeResponse> me() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var token = preferences.getString(Constant.token);
+    final response = await _client.get(Uri.parse("http://localhost:8000/me"),
+        headers: {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhYzExYjQ0Mi04MTMzLTExOWUtODE4MS0zMzkzN2Q1YTAwMDEiLCJpYXQiOjE2NTQ1OTY3NTUsIm5vbWJyZSI6IlJvc2EgTWFyaWEiLCJhcGVsbGlkb3MiOiJSb2RyaWd1ZXogTW92aWxsYSIsInJvbGUiOiJVU1VBUklPIn0.Tz-75PDRl6iiBm5dk-PKgsGOrbi55N-9fpZlGFiUdvZ9eBKSk5oM-jwhlLE9KpCL'});
+    if (response.statusCode == 200) {
+      return MeResponse.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Fail to load me');
     }
   }
 }
